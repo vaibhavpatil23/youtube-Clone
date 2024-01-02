23367125fa383f7ff85869f0fe46440a41f21fc5
vaibhav <vpatil7382@gmail.com>
2024-01-02 11:34:03 +0530

    initial commit

---
+++ src/shared/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loader = () => {
+    return (
+        <div class="load-bar">
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
+    );
+};
+
+export default Loader;
