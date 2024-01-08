df278a198f9adf20fd9c998c61f6ff7732a41973
vaibhav <vpatil7382@gmail.com>
2024-01-08 18:58:11 +0530

    minner chang

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -7,43 +7,37 @@ import LeftNav from "./LeftNav";
 import SearchResultVideoCard from "./SearchResultVideoCard";
 
 const SearchResult = () => {
-    const [result, setResult] = useState();
-    const { searchQuery } = useParams();
-    const { setLoading } = useContext(Context);
+  const [result, setResult] = useState();
+  const { searchQuery } = useParams();
+  const { setLoading } = useContext(Context);
 
-    useEffect(() => {
-        document.getElementById("root").classList.remove("custom-h");
-        fetchSearchResults();
-    }, [searchQuery]);
+  useEffect(() => {
+    document.getElementById("root").classList.remove("custom-h");
+    fetchSearchResults();
+  }, [searchQuery]);
 
-    const fetchSearchResults = () => {
-        setLoading(true);
-        fetchDataFromApi(`search/?q=${searchQuery}`).then((res) => {
-            console.log(res);
-            setResult(res?.contents);
-            setLoading(false);
-        });
-    };
-
-    return (
-        <div className="flex flex-row h-[calc(100%-56px)]">
-            <LeftNav />
-            <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
-                <div className="grid grid-cols-1 gap-2 p-5">
-                    {result?.map((item) => {
-                        if (item?.type !== "video") return false;
-                        let video = item.video;
-                        return (
-                            <SearchResultVideoCard
-                                key={video.videoId}
-                                video={video}
-                            />
-                        );
-                    })}
-                </div>
-            </div>
+  const fetchSearchResults = () => {
+    setLoading(true);
+    fetchDataFromApi(`search/?q=${searchQuery}`).then((res) => {
+      console.log(res);
+      setResult(res?.contents);
+      setLoading(false);
+    });
+  };
+  return (
+    <div className="flex flex-row h-[calc(100%-56px)]">
+      <LeftNav />
+      <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+        <div className="grid grid-cols-1 gap-2 p-5">
+          {result?.map((item) => {
+            if (item?.type !== "video") return false;
+            let video = item.video;
+            return <SearchResultVideoCard key={video.videoId} video={video} />;
+          })}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SearchResult;
